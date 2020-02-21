c5937bcdcca354ec4d03ecc353eb65df6bef5399
Matthieu Baralle <matthieu.baralle@macq.eu>
2020-02-21 16:20:26 +0100

    use XHR request for contact form

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,20 @@
 function sendMessage() {
-    var name = document.getElementById("contact_form_name");
-    var email = document.getElementById("contact_form_email");
-    var message = document.getElementById("contact_form_message");
-
-    var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    
-    if (name.value != null && name.value != "" &&
-    	emailReg.test(email.value) &&
-    	message.value != null && message.value != "") {
-        document.getElementById("submit_contact_form").click();
-
-    	name.value = null;
-    	email.value = null;
-    	message.value = null;
-
-    	document.getElementById("contact_form_error").style.display = "none";
-    	document.getElementById("contact_form_return").style.display = "block";
-    } else {
-    	document.getElementById("contact_form_return").style.display = "none";
-    	document.getElementById("contact_form_error").style.display = "block";
-    }
+	const XHR = new XMLHttpRequest();
+	const FD = new FormData(form);
+	XHR.addEventListener("load", function(event) {
+		alert(event.target.response.getElementsByTagName("h1")[0].innerText);
+		if (XHR.status === 200) {
+			alert("Message sent");
+		} else {
+			alert("Failed to send message");
+		}
+		
+	});
+	XHR.addEventListener("error", function(event) {
+		alert('Oops! Something went wrong.');
+	});
+	
+	XHR.open("POST", "https://formspree.io/xqkqnkvo");
+	XHR.responseType = "document";
+	XHR.send(FD);
 }
